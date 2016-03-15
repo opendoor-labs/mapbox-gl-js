7a260bbe3585bbe29dec5a0ef5b2fdc1da2861f4
Ansis Brammanis <brammanis@gmail.com>
2016-03-14 19:11:59 -0700

    fix querying after reloading vector tiles

--- js/source/worker_tile.js
+++ js/source/worker_tile.js
@@ -20,7 +20,7 @@ function WorkerTile(params) {
     this.collisionDebug = params.collisionDebug;
 }
 
-WorkerTile.prototype.parse = function(data, layers, actor, callback, rawTileData) {
+WorkerTile.prototype.parse = function(data, layers, actor, rawTileData, callback) {
 
     this.status = 'parsing';
     this.data = data;
