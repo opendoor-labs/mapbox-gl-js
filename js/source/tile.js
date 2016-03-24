6e8bceff867ea4a1d428cd09b8c61144f2e5f2d9
Ansis Brammanis <brammanis@gmail.com>
2016-03-23 18:44:18 -0700

    rename FeatureTree to FeatureIndex
    and split intersection tests into a separate file

--- js/source/tile.js
+++ js/source/tile.js
@@ -2,7 +2,7 @@
 
 var util = require('../util/util');
 var Bucket = require('../data/bucket');
-var FeatureTree = require('../data/feature_tree');
+var FeatureIndex = require('../data/feature_index');
 var vt = require('vector-tile');
 var Protobuf = require('pbf');
 var GeoJSONFeature = require('../util/vectortile_to_geojson');
@@ -84,7 +84,7 @@ Tile.prototype = {
 
         this.collisionBoxArray = new CollisionBoxArray(data.collisionBoxArray);
         this.collisionTile = new CollisionTile(data.collisionTile, this.collisionBoxArray);
-        this.featureTree = new FeatureTree(data.featureTree, data.rawTileData, this.collisionTile);
+        this.featureIndex = new FeatureIndex(data.featureIndex, data.rawTileData, this.collisionTile);
         this.rawTileData = data.rawTileData;
         this.buckets = unserializeBuckets(data.buckets);
     },
@@ -101,7 +101,7 @@ Tile.prototype = {
         if (this.isUnloaded) return;
 
         this.collisionTile = new CollisionTile(data.collisionTile, this.collisionBoxArray);
-        this.featureTree.setCollisionTile(this.collisionTile);
+        this.featureIndex.setCollisionTile(this.collisionTile);
 
         // Destroy and delete existing symbol buckets
         for (var id in this.buckets) {
